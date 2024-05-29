096cc3cc23a393e4f78827938479c1cde6ed27c7
kailashniwal09 <niwalkailash1204@outlook.com>
2024-05-29 16:56:05 +0530

    first comment

---
+++ src/Programms_java/evennumber.java
@@ -0,0 +1,18 @@
+package Programms_java;
+
+public class evennumber {
+
+	public static void main(String[] args) {
+
+		int num = 11;
+
+		if (num % 2 == 0) {
+
+			System.out.println("number is even");
+		} else {
+			System.out.println("number is odd");
+		}
+
+	}
+
+}
